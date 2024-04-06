9f34255f67dc44aebc6f2a74e24bbb63e7398863
chrisjames1724 <christopher.cummings28@gmail.com>
2024-04-06 15:45:25 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const inquirer = require("inquirer@8.2.4");
 const fs = require("fs");
 
-
 const questions = [
-{
-    type:"list",
+  {
+    type: "list",
     message: "What would you like to do?",
     name: "choices (change later?",
-    choices: ["View all Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"]
-},
-{
+    choices: [
+      "View all Employees",
+      "Add Employee",
+      "Update Employee Role",
+      "View All Roles",
+      "Add Role",
+      "View All Departments",
+      "Add Department",
+    ],
+  },
+  {},
+];
 
-
-
-}
-
-
-]
-
-
-function init() {
-    
-}
+function init() {}
